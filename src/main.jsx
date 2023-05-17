4ebfcfc0d070c3786af59f492a25417a1c825b2b
imransarkerweb <imransarker.web@gmail.com>
2023-05-17 21:33:28 +0600

    basic header and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <div className="mx-auto container">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
 );
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>just home</div>;
+};
+
+export default Home;
