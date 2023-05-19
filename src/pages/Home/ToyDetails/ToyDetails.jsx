cc8ef4f25279de3d8c5bd592d4e6faeb81442704
imransarkerweb <imransarker.web@gmail.com>
2023-05-20 01:40:25 +0600

    Toydetails page updated

--- src/pages/Home/ToyDetails/ToyDetails.jsx
+++ src/pages/Home/ToyDetails/ToyDetails.jsx
@@ -1,8 +1,61 @@
 import { useLoaderData } from "react-router-dom";
+import ReactStarsRating from "react-awesome-stars-rating";
 
 const ToyDetails = () => {
   const toy = useLoaderData();
-  return <div>detils will be here {toy.toyName}</div>;
+  const {
+    toyName,
+    photo,
+    sellerEmail,
+    sellerName,
+    rating,
+    desciption,
+    qty,
+    price,
+  } = toy;
+  return (
+    <div className="card my-20 lg:card-side bg-base-100 shadow-xl">
+      <figure className="md:w-1/2 mx-auto">
+        <img src={photo} alt="Album" />
+      </figure>
+      <div className="card-body lg:w-1/2">
+        <h2 className="card-title text-4xl mb-6 mx-auto">{toyName}</h2>
+
+        <div className="flex justify-between">
+          <h1 className="badge badge-success  my-3 p-4 text-lg">
+            Price: ${price}
+          </h1>
+          <h1 className="badge badge-ghost  my-3 p-4 text-lg">
+            Quantity: {qty} pc(s)
+          </h1>
+        </div>
+        <p>
+          <span className="font-bold">Details: </span>
+          {desciption}
+        </p>
+        <div className="flex flex-col mt-5 md:flex-row items-center">
+          <p className="flex mb-4 md:mb-0 items-center gap-2">
+            <span className="badge badge-warning p-3 "> Ratings:</span>
+            <ReactStarsRating
+              className="flex"
+              isEdit={false}
+              value={parseFloat(rating)}
+            />
+          </p>
+          <div>
+            {" "}
+            <h2 className="card-title ms-auto">Seller: {sellerName}</h2>
+            <span className="badge ms-auto badge-ghost badge-sm">
+              {sellerEmail}
+            </span>
+          </div>
+        </div>
+        <button className="btn mx-auto mt-6 btn-wide border-none bg-error hover:bg-error-content  transition-all duration-300 ring-offset-2 animate-pulse">
+          Buy Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ToyDetails;
