7fc473f8a9160d9c5647d6eaf0cceee0551c1428
imransarkerweb <imransarker.web@gmail.com>
2023-05-20 03:31:28 +0600

    service card updated

--- src/pages/Home/ServiceCard/ServiceCard.jsx
+++ src/pages/Home/ServiceCard/ServiceCard.jsx
@@ -1,13 +1,21 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 const ServiceCard = () => {
   return (
     <div className="space-y-4 md:space-y-0 mx-auto md:flex justify-around  my-6">
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img
+          {/* <img
             src="https://i.ibb.co/Gkf93x5/lego-3501431-960-720.jpg"
             alt="Delivery Truck"
             className="rounded-xl"
-          />
+          /> */}
+          <Player
+            autoplay
+            loop
+            src="https://assets10.lottiefiles.com/packages/lf20_eop7ymay.json"
+            style={{ height: "200px", width: "200px" }}
+          ></Player>
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title"> Free Delievery!</h2>
@@ -18,11 +26,18 @@ const ServiceCard = () => {
       {/* card no 2  */}
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img
+          {/* <img
             src="https://i.ibb.co/h73StMh/business-3948253-1280.jpg"
             alt="Money Back"
             className="rounded-xl "
-          />
+          /> */}
+
+          <Player
+            autoplay
+            loop
+            src="https://assets9.lottiefiles.com/packages/lf20_jOH2sn.json"
+            style={{ height: "200px", width: "200px" }}
+          ></Player>
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Garaunteed Moneyback!</h2>
@@ -33,11 +48,17 @@ const ServiceCard = () => {
       {/* three */}
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure className="px-10 pt-10">
-          <img
+          {/* <img
             src="https://cdn.pixabay.com/photo/2018/11/21/21/54/bunny-3830669_960_720.jpg"
             alt="Payment"
             className="rounded-xl"
-          />
+          /> */}
+          <Player
+            autoplay
+            loop
+            src="https://assets6.lottiefiles.com/packages/lf20_yzoqyyqf.json"
+            style={{ height: "200px", width: "200px" }}
+          ></Player>
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">Payment Options!</h2>
